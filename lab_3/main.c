d61cf0d85888b84c67dfe427ba223496eeda8cb4
Hunter <h.gross1234@gmail.com>
2019-02-16 14:44:32 -0500

    Hunter changed the system calls so that they activate the correct leds

--- lab_3/main.c
+++ lab_3/main.c
@@ -1,5 +1,5 @@
 /*
-ECE477 Lab 3: LED CPU load monitor. 
+ECE477 Lab 3: LED CPU load monitor.
 
 Authors: Steph Poirier, Hunter Gross, Zach Haviland
 Spring 2019
@@ -36,33 +36,33 @@ int main(int argc, char *argv[]){
 
 
 	while(1) {
-		
+
 		fp = fopen("/proc/loadavg","r");
-		
+
 		/* Error checking */
 		if(fp == NULL){
 			printf("error opening file, exiting.");
 			return 1;
 		}
-		
+
 		//First value encountered is minute load average
 		fscanf(fp, "%lf", &fileval);
 
 		printf("%.3f\n",fileval); ///For debugging
 
 		if(fileval >= 8){
-			//turn leds <=2 on
-			system("./ledctrl 0x03");
+			//turn all leds on
+			system("./ledctrl 0xff");
 		}
 
 		else if(fileval >= 4) {
-			//turn leds <=3 on
-			system("./ledctrl 0x07");
+			//turn leds <=7 on
+			system("./ledctrl 0x7f");
 		}
 
 		else if(fileval >= 2) {
-			//turn leds <=4 on
-			system("./ledctrl 0x0f");
+			//turn leds <=6 on
+			system("./ledctrl 0x3f");
 		}
 
 		else if(fileval >= 1) {
@@ -71,30 +71,31 @@ int main(int argc, char *argv[]){
 		}
 
 		else if(fileval >= 0.5) {
-			//turn leds <=6 on
-			system("./ledctrl 0x3f");
+			//turn leds <=4 on
+			system("./ledctrl 0x0f");
 		}
 
 		else if(fileval >= 0.25) {
-			//turn led <=7 on
-			system("./ledctrl 0x7f");
+			//turn leds <=3 on
+			system("./ledctrl 0x07");
 		}
 
 		else if(fileval >= 0.125) {
-			//turn all leds on
-			system("./ledctrl 0xff");
+			//turn leds <= 2 on
+			system("./ledctrl 0x03");
 		}
 		else{
+			//turn led 1 on
 			system("./ledctrl 0x01");
 		}
 		/* reset pointer to the start of the file */
 		//rewind(fp);
-		
+
 		//closes file and checks to make sure it succeded
 		if(fclose(fp)){
 			printf("failed to close /proc/loadavg. Aborting program");
 			return 1;
-		}		
+		}
 
 	}
 }
