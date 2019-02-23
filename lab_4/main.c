96ed96b37113e81e7f5f1a53eceacbef9f23aafb
Hunter <hunter.gross@maine.edu>
2019-02-23 14:14:11 -0500

    Hunter wrote main.c header comment

--- lab_4/main.c
+++ lab_4/main.c
@@ -1,10 +1,20 @@
 /*
-ECE477 Lab 4
-
-
-
-
-
-
-
+ * ECE477 Lab 4: Push Button Interface 
+ * 
+ * Authors: Zach Haviland, Hunter Gross, Stephanie Poirier
+ * 
+ * Spring 2019
+ *
+ * This program utilizes two push buttons (A and B) which control the speed and direction
+ * of a shifting LED. The program runs continuously until both buttons are pressed at the same time.
+ * Only one LED is lit at a time, the light moves from the least significant position to 
+ * the most significant position and wraps around when it reaches the end. 
+ *
+ * Push button A halves the delay between switching LEDs. The lower bound of the delay
+ * is 32 milliseconds. Push button B doubles the delay. The upper bound of the delay is
+ * 1024 milliseconds.
+ * Pressing a button after it has reached its respective bound does not change speed, but reverses
+ * the direction of the traveling light. 
+ *
+ * The code executes on button press, not release. Holding the button doesn't change code behavior.
  */
