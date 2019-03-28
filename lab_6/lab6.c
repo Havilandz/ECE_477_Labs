be1274ba4aa60619e2158e657e029b0abb799380
Hunter <hunter.gross@maine.edu>
2019-03-28 15:08:56 -0400

    Hunter added lab6.c

---
+++ lab_6/lab6.c
@@ -0,0 +1,22 @@
+/* 
+ * Lab 6 source code
+ * Authors: Hunter Gross, Zach Haviland, Stephanie Poirier
+ *
+ * HEADER COMMENT HERE
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
