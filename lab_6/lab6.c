dab9dbcac441f24e69d4f70e93f4e108133d2946
Hunter <hunter.gross@maine.edu>
2019-03-28 19:21:55 -0400

    Hunter added forking as an attempt to fix issues with polling keyboard and serial at the same time.

--- lab_6/lab6.c
+++ lab_6/lab6.c
@@ -21,8 +21,9 @@ int main(int argc, char* argv[])
 	/* Buffer where key presses get stored */
 	unsigned char keys[KEY_BUFF_SIZE];
 	
-	/* file descriptor for serial and ready flag for select() */
-	int fd, ready, i=0;
+	/* file descriptor for serial and ready flag for select()
+	index for key buffer and process id */
+	int fd, ready, i=0, pid;
 
 	/* Buffer where data from serial is stored */
 	unsigned char readOut[READ_BUFF_SIZE];
@@ -38,41 +39,49 @@ int main(int argc, char* argv[])
 		printf("NULL POINTER! ttyS0 failed to open\n");
 		return -1;
 	}
-
-	/* fills the fd_set structs */
-	FD_SET(fd, &write_set); // write to serial
-	FD_SET(fd, &read_set); // read from serial 
-	FD_SET(0, &read_set); // read from stdin
-
-	while (1) {
-
-		/* select waits for a file descriptor to be ready to use */
-		ready = select(2, &read_set, &write_set, NULL, NULL); 	
-
-		/* select throws -1 for errors */
-		if (ready == -1) {
-			printf("Error with select()\n");
+		pid = fork();
+		
+		if (pid == -1) {
+			printf("Fork failed, exiting...\n");
 			return -1;
 		}
-		/* select throws 0 when it times out */
-		else if (ready == 0) {
-			printf("select() timed out\n");
-			return 1;
+		if (pid) {
+			/* fills the fd_set structs */
+			FD_SET(fd, &write_set); // write to serial
+			FD_SET(fd, &read_set); // read from serial 
+
+
+			/* select waits for a file descriptor to be ready to use */
+			ready = select(2, &read_set, &write_set, NULL, NULL); 	
+
+			/* select throws -1 for errors */
+			if (ready == -1) {
+				printf("Error with select()\n");
+				return -1;
+			}
+			/* select throws 0 when it times out */
+			else if (ready == 0) {
+				printf("select() timed out\n");
+				return 1;
+			}
+			/* if select is successful */
+			else {
+				if (FD_ISSET(fd, &read_set)) { // Serial read is ready
+					do {
+						read(fd, &readOut, 1);
+					}while (*readOut != '\0'); // Assumes data is null terminated
+					return 0;
+				}
+				if (FD_ISSET(fd, &write_set)) { // Serial write is ready
+					write(fd, &keys, 1);
+					return 0;
+				}
+			}
 		}
-		/* if select is successful */
-		else {
-			if (FD_ISSET(fd, &read_set)) { // Serial read is ready
-				do {
-					read(fd, &readOut, 1);
-				}while (*readOut != '\0'); // Assumes data is null terminated
-			}
-			if (FD_ISSET(1, &read_set)) { // stdin is ready for a key
-				keys[++i] = (unsigned char)getchar();
-			}
-			if (FD_ISSET(fd, &write_set)) { // Serial write is ready
-				write(fd, &keys, 1);
+		if (!pid) {
+			while (1) {
+				keys[++i] = getchar();
 			}
 		}
-	}	
 	return 0;
 }
