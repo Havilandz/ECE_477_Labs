3b5ce3d90d8bb6bf7aaf6227e79fc06bfd87e3b3
Hunter <hunter.gross@maine.edu>
2019-03-28 20:08:02 -0400

    Hunter reverted previous commit, wrote helper function to open/close file, current issue: can't set up select without opening the file + can't close the file without deleting fd

--- lab_6/lab6.c
+++ lab_6/lab6.c
@@ -16,6 +16,10 @@
 #include<fcntl.h>
 #define READ_BUFF_SIZE 50
 #define KEY_BUFF_SIZE 10
+#define STDIN 0
+
+int accessSerial(int n, int fd);
+
 int main(int argc, char* argv[])
 {
 	/* Buffer where key presses get stored */
@@ -31,57 +35,61 @@ int main(int argc, char* argv[])
 	/* fd_set structs for select() */
 	fd_set read_set, write_set;
 			
-	/* Open the serial port */
-	fd = open("/dev/ttyS0",O_RDWR);
+	/* fills the fd_set structs */
+	FD_SET(fd, &write_set); // write to serial
+	FD_SET(fd, &read_set); // read from serial 
+	FD_SET(STDIN, &read_set); // read from stdin
+	
+	while (1) {
+		/* select waits for a file descriptor to be ready to use */
+		ready = select(2, &read_set, &write_set, NULL, NULL); 	
 
-	/* Error checking open() */
-	if (!fd) {
-		printf("NULL POINTER! ttyS0 failed to open\n");
-		return -1;
-	}
-		pid = fork();
-		
-		if (pid == -1) {
-			printf("Fork failed, exiting...\n");
+		/* select throws -1 for errors */
+		if (ready == -1) {
+			printf("Error with select()\n");
 			return -1;
 		}
-		if (pid) {
-			/* fills the fd_set structs */
-			FD_SET(fd, &write_set); // write to serial
-			FD_SET(fd, &read_set); // read from serial 
-
-
-			/* select waits for a file descriptor to be ready to use */
-			ready = select(2, &read_set, &write_set, NULL, NULL); 	
-
-			/* select throws -1 for errors */
-			if (ready == -1) {
-				printf("Error with select()\n");
-				return -1;
-			}
-			/* select throws 0 when it times out */
-			else if (ready == 0) {
-				printf("select() timed out\n");
-				return 1;
-			}
-			/* if select is successful */
-			else {
-				if (FD_ISSET(fd, &read_set)) { // Serial read is ready
-					do {
-						read(fd, &readOut, 1);
-					}while (*readOut != '\0'); // Assumes data is null terminated
-					return 0;
-				}
-				if (FD_ISSET(fd, &write_set)) { // Serial write is ready
-					write(fd, &keys, 1);
-					return 0;
-				}
-			}
+		/* select throws 0 when it times out */
+		else if (ready == 0) {
+			printf("select() timed out\n");
+			return 1;
 		}
-		if (!pid) {
-			while (1) {
+		/* if select is successful */
+		else {
+			if (FD_ISSET(fd, &read_set)) { // Serial read is ready
+				accessSerial(0,fd);
+				do {
+				read(fd, &readOut, 1);
+				}while (*readOut != '\0'); // Assumes data is null terminated
+				accessSerial(1,fd);
+			}
+			if (FD_ISSET(fd, &write_set)) { // Serial write is ready
+				accessSerial(0,fd);
+				write(fd, &keys, 1);
+				accessSerial(1,fd);
+			}
+			if(FD_ISSET(STDIN, &readOut)) { // stdin read is ready	
 				keys[++i] = getchar();
 			}
 		}
+	}
 	return 0;
+}
+
+int accessSerial(int n, int fd)
+{
+	if (n == 0) {
+
+		fd = open("/dev/ttyS0",O_RDWR);
+	
+		if (!fd) {
+			printf("NULL POINTER! ttylS0 failed to open\n");
+			return -1;
+		}
+		return fd;
+	}
+	if (n == 1) {
+		close(fd);
+		return 0;
+ 	}
 }
