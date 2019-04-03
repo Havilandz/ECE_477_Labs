a684653636652a729bc3ada5753de5802544c4ea
Hunter <hunter.gross@maine.edu>
2019-04-03 17:50:59 -0400

    Hunter added comments to main.c and led.c/.h

--- lab_7/avr/led.h
+++ lab_7/avr/led.h
@@ -1,7 +1,20 @@
+/*
+ * AVR LED Interface
+ * Authors: Hunter Gross, Zach Haviland, Stephanie Poirier
+ *
+ * The functions included configure a set of 8 LEDs wired to pins 0-7
+ * on PORTC of the ATMEGA324PA AVR.
+ */
+
 #ifndef LED_H
 #define LED_H
 
+/* Initializes PORTC and tests
+the LEDs by toggling them */
 void initLEDs(void);
-void setLEDs(int led);
+
+/* Sets the LEDs correspoding to 
+an 8 bit integer passed as an argument */
+void setLEDs(uint8_t led);
 
 #endif
--- lab_7/avr/main.c
+++ lab_7/avr/main.c
@@ -1,3 +1,15 @@
+/*
+ * Lab 7 AVR Main Routine
+ * Authors: Hunter Gross, Zach Haviland, Stephanie Poirier
+ *
+ * This program reads from serial to obtain user input from stdin in
+ * the form of an integer corresponding to a hexadecimal value to 
+ * control a set of 8 LEDs. After sending the data to setLEDs, the program
+ * takes a meaurement of the power supply voltage and sends it through serial
+ * communication to be printed to the terminal.
+ */
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -8,23 +20,31 @@
 
 void main(int argc, char* argv[])
 {
+	FILE *input, *output; 	// File pointers for stdin and stdout
+	int n = 0; 		// Integer for reading from stdin
+	double measurement; 	// Power supply measurement to be taken
 
+	/* Serial communication setup */
 	serial_init();
 	_delay_ms(3000);
 		
-
-	FILE *input, *output;
-	int n = 0;
-	double measurement;
+	/* External function initialization */
 	initLEDs();
 	initPS();
+	
 	input = stdin;
 	output = stdout;
+	
 	while(1) {
-		fscanf(input, "%i", &n);
+		fscanf(input, "%i", &n); // Read from serial
+
+		/* Formats n into uint8_t to prevent unintended behavior */
 		n &= 0xff00;
 		n = (uint8_t)n;
+		
 		setLEDs(n);
+		
+		/* Takes the supply voltage measurement and sends it to the terminal */
 		measurement = measurePS();
 		fprintf(output, "The power rail is at %d Volts\r\n", measurement);
 	}
