9f78092524033c59f60c6f354c8ffa73b985c3ab
Hunter <hunter.gross@maine.edu>
2019-04-07 18:53:26 -0400

    Hunter's end of day push

--- project/stacker.c
+++ project/stacker.c
@@ -6,9 +6,17 @@
  *
  */
 
-#include "stacker.h"
 #include "spi/spi.h"
 #include <stdio.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#define BOUNCE_DELAY 500
+
+int yLocation = 0;
+uint8_t currentRow;
+uint8_t prevRow = 0xff;
+
 int main(int argc, char* argv[])
 {
 	/* Initialization */
@@ -19,6 +27,17 @@ int main(int argc, char* argv[])
 	while(1) {
 		
 	}
+
+}
+
+
+ISR(INT0_vect)
+{
+	_delay_ms(BOUNCE_DELAY);
+	yLocation += 2;
+
+	currentRow &= prevRow;	
+	prevRow = currentRow;
 
 }
 
