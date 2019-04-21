1b404226a6d05d4ee4b000b33b524141245f993e
Zach <ZmanX102@gmail.com>
2019-04-21 12:17:59 -0400

    added some abstraction to tesing file

--- project/spi/test.c
+++ project/spi/test.c
@@ -25,17 +25,25 @@ int main(int argc, char* argv[])
 	USART_SPI_init();
 	_delay_ms(500);
 	output = stdout;
-	fwrite(&no_decode, 2, 1, output);
-	while(1){
-		setLEDs(0x00);
-		fwrite(&msg, 2, 1, output);		
-		setLEDs(0x01);
-		_delay_ms(500);
-		setLEDs(0x00);
-		fwrite(&msg2, 2, 1, output);		
-		setLEDs(0x01);
-		_delay_ms(500);
-
-	}
+	while(1);
 }
+
+void max719_init(void){
+	spi_write(0x09, 0); //no decode
+	spi_write(0x0A, 1); //intensity 3
+	spi_write(0x0B, 7); // scan limit: akk "digits" rows (on)
+	spi_write(0x0C, 1);
+	spi_write(0x0F, 0);
+	
+}
+void spi_write(int addr, int data);
+	
+	uint16_t msg = addr<<8 + data;
+	setLEDs(0x00);
+	fwrite(&msg, 2, 1, output);
+	setLEDs(0x01);
+
+
+}
+
 
